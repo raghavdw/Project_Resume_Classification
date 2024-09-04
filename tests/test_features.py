6735372b97cdb3162d4185fe376ebfc62571c29c
Raghav Wate <raghavwate@minus-MacBook-Pro.local>
2024-09-04 13:20:19 -0700

    fixed test_features.py for number of features generated

--- tests/test_features.py
+++ tests/test_features.py
@@ -2,4 +2,4 @@ from resume_model.processing.features import extract_features
 
 def test_extract_features(sample_data):
     features, _ = extract_features(sample_data)
-    assert features.shape[1] == 3000
+    assert features.shape[1] == 1
