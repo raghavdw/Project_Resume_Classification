911b5592dd563306ab1f84b9385660c68e521d74
Raghav Wate <raghavwate@minus-MacBook-Pro.local>
2024-09-04 13:39:46 -0700

    Updated test_predictions.py

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -1,5 +1,7 @@
 from resume_model.predict import predict_resume
 
 def test_predict_resume():
+    """ sample prediction function"""
+    
     result = predict_resume("law and order")
     assert result is not None
